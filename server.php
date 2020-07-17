cb506c32f0b266e886d599f54a28ada841c0f5e4
lindam <linda.minotti2@gmail.com>
2020-07-17 15:08:30 +0200

    Bonus.

--- server.php
+++ server.php
@@ -3,7 +3,21 @@
   @include __DIR__ . '/database.php';
   // lo trasfromo in json
   $database_jason = json_encode($database);
+
+
+  // echo $_GET['author'];
+  // Seleziono in base all'autore scelto dall'utente
+  $database_author = [];
+  foreach ($database as $disk) {
+
+    if ($_GET['author'] === $disk['author']) {
+
+
+    }
+  }
+
+
   // dico che do un output in json
   header('Content-Type: application/json');
-  echo($database_jason);
+  // echo($database_jason);
 ?>
